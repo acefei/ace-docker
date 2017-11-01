4e3d52e0f0844ec8283c0ffc7121dc715848bfe5
acefei <acefeilxy@gmail.com>
2017-11-01 17:09:21 +0800

    add requirements.txt for scrapyd

--- scrapyd/test.sh
+++ scrapyd/test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #docker run -it acefei/scrapyd /bin/bash
-docker run -p 6800:6800 -p 5000:5000 -v /tmp/scrapyd-data:/data acefei/scrapyd 
+docker run -d -p 6800:6800 -p 5000:5000 -v /tmp/scrapyd-data:/data acefei/scrapyd 
