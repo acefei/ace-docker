36e498bfb286d3a1a337b3cc7a64d5b8046cd391
acefei <acefeilxy@gmail.com>
2017-11-10 10:10:57 +0000

    add README for redis

--- redis/test.sh
+++ redis/test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run -p 6379:6379 -v /tmp/redis-data:/data --name redis acefei/redis
+
+docker run -d -p 6379:6379 -v /tmp/redis-data:/data acefei/redis redis-server /etc/redis/redis.conf --requirepass $1
