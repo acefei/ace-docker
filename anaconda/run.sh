f99f2a9e4885efaf89d25657a8184f3cecb96f80
acefei <acefeilxy@gmail.com>
2017-11-22 16:26:35 +0800

    add command for running jupyter by root user

--- anaconda/run.sh
+++ anaconda/run.sh
@@ -1,2 +1,4 @@
 port=${1:-8888}
-docker run -i -t -p $port:$port continuumio/anaconda /bin/bash -c "mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=$port --no-browser --NotebookApp.token=''"
+docker run -i -t -d -p $port:$port continuumio/anaconda /bin/bash -c "mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=$port --no-browser --NotebookApp.token=''"
+# for root user
+#docker run -i -t -d -p $port:$port continuumio/anaconda /bin/bash -c "mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=$port --no-browser --NotebookApp.token='' --allow-root"
