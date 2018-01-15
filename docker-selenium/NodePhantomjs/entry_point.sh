03691d0e2ce85abdfa99bde0b90ba96ca48a91c5
acefei <acefeilxy@gmail.com>
2018-01-15 18:28:44 +0800

    add NodePhantomjs in docker-selenium

---
+++ docker-selenium/NodePhantomjs/entry_point.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source /opt/bin/functions.sh
+
+if [ -z "$HUB_PORT_4444_TCP_ADDR" ]; then
+  echo Not linked with a running Hub container 1>&2
+  exit 1
+fi
+
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
+if [ -z "$IP" ]; then
+  IP="$(hostname -i)"
+fi
+
+if [ -z "$PORT" ]; then
+  PORT="4444"
+fi
+
+phantomjs --webdriver=$IP:$PORT ${PHANTOMJS_OPTS} --webdriver-selenium-grid-hub=http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT
+
+trap shutdown SIGTERM SIGINT
+wait $NODE_PID
